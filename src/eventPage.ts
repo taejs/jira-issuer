42e3ab870a92cd5775b6ddeb54fc2dc4b4ba542c
Tae <smireat1@gmail.com>
2019-07-29 23:45:49 +0900

    finally float a dialog on document.body
    copy examples posted on https://itnext.io/create-chrome-extension-with-reactjs-using-inject-page-strategy-137650de1f39

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -9,3 +9,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     return isResponseAsync;
 });
+
+// Called when the user clicks on the browser action
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow:true},
+        function(tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id,
+                {"message": "clicked_browser_action"}
+            );
+        });
+});
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,10 +2,27 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Popup from './Popup';
 
-chrome.tabs.query({ active: true, currentWindow: true }, tab => {
-    ReactDOM.render(<Popup />, document.getElementById('popup'));
-    // const app = document.createElement('div');
-    // app.id = "my-extension-root";
-    // document.body.appendChild(app);
-    // ReactDOM.render(<Popup />, app);
-});
+function toggle(){
+    if(app.style.display === "none"){
+        app.style.display = "block";
+    }else{
+        app.style.display = "none";
+    }
+}
+
+// ReactDOM.render(<Popup />, document.getElementById('popup'));
+const app = document.createElement('div');
+
+//app.style.display = "none";
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "clicked_browser_action") {
+            toggle();
+        }
+    }
+);
+
+app.id = "my-extension-root";
+console.log('----------------'+document.body);
+document.body.appendChild(app);
+ReactDOM.render(<Popup />, app);
