f25b993bd4d01486208dec489b5c75185670983e
Tae <smireat1@gmail.com>
2019-08-02 00:24:41 +0900

    add formgroup for test

--- src/popup/ModalIssueCreate.tsx
+++ src/popup/ModalIssueCreate.tsx
@@ -4,12 +4,15 @@ import Button, { ButtonGroup } from '@atlaskit/button'
 import ModalDialog, { ModalTransition } from '@atlaskit/modal-dialog';
 import Form, { Field, CheckboxField } from '@atlaskit/form';
 import TextField from '@atlaskit/textfield';
+import Select from '@atlaskit/select';
+
 
 interface AppProps {
     onClose : (e : any) => void
 }
 
 interface AppState {
+    service : string
 }
 
 
@@ -60,7 +63,42 @@ export default class ModalIssueCreate extends React.Component<AppProps, AppState
                 }}
                 >
                 <p>Enter some text then submit the form to see the response</p>
-                <Field label="iss"/>
+                <Field
+                    name="summary"
+                    label="summary"
+                    defaultValue=""
+                    isRequired
+                    validate={value=> (value.length < 8 ? 'TOO_SHORT' : undefined)}>
+                    <TextField autoComplete="off"/>
+                </Field>
+                <Field
+                    name="서비스&메뉴"
+                    label="서비스&메뉴">
+                    <Select
+                        className="single-select"
+                        classNamePrefix="react-select"
+                        options={[
+                        { label: 'None', value: '' },
+                        { label: '[2.0] 관리자사이트', value: '10223'},
+                        { label: '[2.0] 평가자사이트', value: '10224'}
+                        ]}
+                        placeholder="서비스"
+                        onChange={service => this.setState({ service })}
+                    />
+                    <Select
+                        className="single-select"
+                        classNamePrefix="react-select"
+                        options={() => {
+                            const map = {
+                                '10223' : [{label : '냠냠', value : '20000'}],
+                                '10224' : [{label : '이력서팝업', value : '22'}]
+                            }
+                            const {service} = this.state;
+                            return map[service] || [];
+                        }}
+                        placeholder="메뉴"
+                    />
+                </Field>
             </ModalDialog>
         )
     }
